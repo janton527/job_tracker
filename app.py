77e8263ab66ba0d1633152735009a6d1ba180b7c
Jacob Stanton <bocaj527@hotmail.com>
2026-03-23 13:56:41 -0400

    add 'add application' page

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, url_for
 import mysql.connector
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 def get_db():
     return mysql.connector.connect(
@@ -28,6 +30,37 @@ def applications():
     conn.close()
 
     return render_template('applications.html', applications=applications)
+
+@app.route('/add_application', methods=["GET", "POST"])
+def add_application():
+    if request.method == "POST": 
+        job_id = request.form['job_id']
+        application_date = request.form['application_date']
+        status = request.form['status']
+        resume_version = request.form['resume_version']
+        cover_letter_sent = 1 if request.form.get('cover_letter_sent') else 0
+
+        try:
+            conn = get_db()
+            cursor = conn.cursor()
+
+            cursor.execute("""
+                INSERT INTO applications 
+                    (job_id, application_date, status, resume_version, cover_letter_sent)
+                VALUES(%s, %s, %s, %s, %s)
+                """, (job_id, application_date, status, resume_version, cover_letter_sent))
+
+            conn.commit()
+
+            flash('Application added successfully!')
+        except Exception as e:
+            flash(f'Error: {e}', 'danger')
+        finally: 
+            conn.close()
+
+        return redirect(url_for('applications'))
+    else:
+        return render_template('add_application.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
