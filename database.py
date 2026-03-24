f7a256ea5aa588ec7bb536860b289dc2eb4d8907
Jacob Stanton <bocaj527@hotmail.com>
2026-03-24 13:01:02 -0400

    add table validation

--- database.py
+++ database.py
@@ -1,5 +1,35 @@
 import mysql.connector
 
+ALLOW_TABLES = {
+        "applications": {
+            "id": "application_id",
+            "fields": [
+                "job_id", "application_date", "status", "resume_version",
+                "cover_letter_sent", "notes", "response_date", "interview_date"
+                ]
+            },
+        "jobs": {
+            "id": "job_id",
+            "fields": [
+                "company_id", "job_title", "job_description", "salary_min",
+                "salary_max", "job_type", "posting_url", "posting_date", 
+                "is_active"
+                ]
+            }, 
+        "companies": {
+            "id": "company_id",
+            "fields": [
+                "company_name", "industry", "website", "city", "state,", "notes"
+                ]
+            },
+        "contacts": {
+            "id": "contact_id",
+            "fields": [
+                "company_id", "first_name", "last_name", "email", "phone", 
+                "job_title", "linkedin_url", "notes"
+                ]
+            }
+        }
 
 class CRUD:
 
@@ -10,6 +40,10 @@ class CRUD:
                 password='Password1!',
                 database='job_tracker'
         )
+
+    def validate_table(self, table): 
+        if table not in ALLOWED_TABLES:
+            raise ValueError("Invalid table")
 
     def fetch_all(self, table):
         conn = self.get_db()
